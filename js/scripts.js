571455bbe54f28d2fa07bc0fe8f9197ddd7d1993
vporsnev <v.porshnev@gmail.com>
2022-03-19 18:30:55 +0100

    Pokemon list added
    3 pokemon arrays added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favFood = 'Spaghetti';
-document.write(favFood);
+let pokemonList = [
+  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+  {name: 'Pikachu', height: 4, types: ['electric']},
+  {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']}
+];
