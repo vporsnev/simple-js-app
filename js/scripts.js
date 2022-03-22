ccf07dc1699bfb7df25beb38852e3efcdb7c0efe
vporsnev <v.porshnev@gmail.com>
2022-03-22 21:30:12 +0100

    Big update
    -Loop created for pokemonList
    -fonts folder with custom pokemon font added
    -some small styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
   {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
   {name: 'Pikachu', height: 4, types: ['electric']},
-  {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']}
+  {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']},
+  {name: 'Snorlax', height: 22, types: ['normal']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5){
+    document.write("<br>" + " " + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " - is a big pokemon!");
+  } else {
+    document.write("<br>" + " " + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")");
+  }
+}
