31d533c87458e2cd64975b3195170b6b8cc36d14
vporsnev <v.porshnev@gmail.com>
2022-03-22 21:34:47 +0100

    Update scripts.js
    test

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,11 @@ let pokemonList = [
   {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
   {name: 'Pikachu', height: 4, types: ['electric']},
   {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']},
-  {name: 'Snorlax', height: 22, types: ['normal']}
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height > 5){
-    document.write("<br>" + " " + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " - is a big pokemon!");
+    document.write("<br>" + " " + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")" + " - Wow, that’s big!");
   } else {
     document.write("<br>" + " " + pokemonList[i].name + " " + "(height:" + " " + pokemonList[i].height + ")");
   }
