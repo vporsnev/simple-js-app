750d62498d83b09f6651c6ac5587b57eb6b5d5b5
vporsnev <v.porshnev@gmail.com>
2022-03-30 19:54:49 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+let pokemonRepository = (function () {
 let pokemonList = [
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-  {name: 'Pikachu', height: 4, types: ['electric']},
-  {name: 'Jigglypuff', height: 5, types: ['fairy', 'normal']},
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  {name: 'Pikachu', height: 0.4, types: ['electric']},
+  {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']},
 ];
 
-pokemonList.forEach(function(pokemon) {
+return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
+pokemonRepository.add({ name: 'Slowpoke', height: 1.2, types: ['psychic', 'water'] };
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write("<br>" + " " + pokemon.name + " " + "(height:" + " " + pokemon.height + ")")
 });
