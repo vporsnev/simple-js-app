8c8829d4e8c6749dc7ba14c3b801f3e6363ad3c3
vporsnev <v.porshnev@gmail.com>
2022-04-27 13:04:52 +0200

    external API added

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
 let pokemonRepository = (function() {
-  let pokemonList = [{
-      name: 'Bulbasaur',
-      height: 0.7,
-      types: ['grass', 'poison']
-    },
-    {
-      name: 'Pikachu',
-      height: 0.4,
-      types: ['electric']
-    },
-    {
-      name: 'Jigglypuff',
-      height: 0.5,
-      types: ['fairy', 'normal']
-    },
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon
+    ) {
+      pokemonList.push(pokemon)
+    } else {
+      console.log('pokemon is not correct');
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
@@ -27,37 +28,60 @@ let pokemonRepository = (function() {
     button.addEventListener('click', function(event) {
       showDetails(pokemon)
     });
-  };
+  }
 
-  function showDetails(pokemon) {
-    console.log(pokemon.name);
-  };
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+
+        console.log(pokemon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function() {
+      console.log(item);
+    });
+  }
 
   return {
-    add: function(pokemon) {
-      if (
-        typeof pokemon === 'object' &&
-        'name' in pokemon &&
-        'height' in pokemon &&
-        'types' in pokemon
-      ) {
-        pokemonList.push(pokemon)
-      } else {
-        console.log('pokemon is not correct');
-      };
-    },
-    getAll: function() {
-      return pokemonList;
-    },
-    addListItem: addListItem
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 })();
 
-pokemonRepository.add({
-  name: 'Slowpoke',
-  height: 1.2,
-  types: ['psychic', 'water']
-});
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+//console.log(pokemonRepository.getAll());
+
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
